62138445d67ec61d5b523b9a5630e8692bf73d2b
eli <atefeh.ramezani.137@gmail.com>
2023-02-10 13:42:20 +0330

    notification added to the panel

--- django_website/core/admin.py
+++ django_website/core/admin.py
@@ -1,20 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-
 from .models import *
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save_and_continue'] = False
+        return super().changeform_view(request, object_id, extra_context=extra_context)
 
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    fields = ('id', 'notif_type', 'value', 'model_created_at')
-    list_display = ['id', 'notif_type', 'value', 'model_created_at']
-    list_display_links = ['id', 'notif_type', 'value', 'model_created_at']
-    readonly_fields = ('id', 'notif_type', 'value', 'model_created_at')
+    fields = ('event_index', 'event_message', 'event_sensor_data', 'model_created_at')
+    list_display = ['event_index', 'event_message', 'model_created_at']
+    list_display_links = ['event_index', 'event_message', 'model_created_at']
+    readonly_fields = ('event_index', 'event_message', 'event_sensor_data', 'model_created_at')
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -30,10 +38,13 @@ class NotificationAdmin(admin.ModelAdmin):
 
 @admin.register(MiddlewareFilter)
 class MiddlewareFilterAdmin(admin.ModelAdmin):
-    fields = ('interval_publish_to_blockchain', 'interval_publish_to_broker', 'model_modified_at')
-    list_display = ['name', 'interval_publish_to_blockchain', 'interval_publish_to_broker', 'model_modified_at']
-    list_display_links = ['name', 'interval_publish_to_blockchain', 'interval_publish_to_broker', 'model_modified_at']
-    readonly_fields = ('name', 'model_modified_at',)
+    fields = ('interval_publish_to_blockchain', 'interval_publish_to_broker', 'low_temp_range', 'high_temp_range',
+              'model_modified_at')
+    list_display = ['name', 'interval_publish_to_blockchain', 'interval_publish_to_broker', 'low_temp_range',
+                    'high_temp_range', 'model_modified_at']
+    list_display_links = ['name', 'interval_publish_to_blockchain', 'interval_publish_to_broker', 'low_temp_range',
+                          'high_temp_range', 'model_modified_at']
+    readonly_fields = ('name', 'model_modified_at')
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -48,3 +59,4 @@ class MiddlewareFilterAdmin(admin.ModelAdmin):
 
 
 admin.site.unregister(Group)
+
