24b3fd1bd7f2ee76d97e7943b96d7a9adf321bee
eli <atefeh.ramezani.137@gmail.com>
2023-02-10 13:07:53 +0330

    updated for getting temp filters

--- utils/database_commands.py
+++ utils/database_commands.py
@@ -15,3 +15,12 @@ def get_interval_publish_to_blockchain_from_sqlite(sqlite_path):
         cursor.execute("SELECT interval_publish_to_blockchain FROM core_middlewarefilter")
         rows = cursor.fetchall()
         return float(rows[0][0])
+
+
+
+def get_low_high_temp_range(sqlite_path):
+    with sqlite3.connect(sqlite_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT low_temp_range, high_temp_range FROM core_middlewarefilter")
+        rows = cursor.fetchall()
+        return float(rows[0][0]), float(rows[0][1])
